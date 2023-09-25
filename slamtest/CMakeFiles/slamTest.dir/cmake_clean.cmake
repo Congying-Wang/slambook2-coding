8ecb7facee818df8bc747dcaffb331cfcbb1a4a3
Congying-Wang <cymobius@gmail.com>
2023-09-25 18:28:02 +0800

    ch3

---
+++ slamtest/CMakeFiles/slamTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slamTest.dir/slamTest.cpp.o"
+  "CMakeFiles/slamTest.dir/slamTest.cpp.o.d"
+  "slamTest"
+  "slamTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slamTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
